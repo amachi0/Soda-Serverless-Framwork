7743a8456a67d9019fde5a6e9d7c4aa53949aa59
天知 <shunsuke.kmkz0617@gmail.com>
2019-03-04 18:29:31 +0900

    :recycle: 大幅なリファクタリング

--- app/functions/api_gateway/event/change_event.py
+++ app/functions/api_gateway/event/change_event.py
@@ -1,76 +1,41 @@
 import json
-import boto3
-import os
-from app.util.decimalencoder import DecimalEncoder
+from app.data.event import Event
+from app.data.source.event_table import EventTable
+from app.util.return_dict import Successed, Failured
 
-dynamodb = boto3.resource('dynamodb')
-eventTableName = os.environ['EVENT_TABLE']
+''' パラメーター
+{
+	"eventId" : 263,
+	"identityId" : "amachi",
+	"urlData" : "https://nangngnainil34982379gsesssgg",
+	"start" : 1526000000,
+	"end" : 1526800000,
+	"university" : "立命館",
+	"eventName" : "プログラミング教室",
+	"price" : "500円",
+	"location" : "BKC アクロスウィング",
+	"qualification" : "初心者でもOK!",
+	"detail" : "Djangoを勉強しよう",
+	"contact" : "amachi@gmail.com",
+	"isPrivate" : false
+}
+'''
 
 def change_event(event, context):
     try:
         param = json.loads(event["body"])
         eventId = param['eventId']
 
-        if not(param['end']):
-            end = None
-        else:
-            end = int(param['end'])
-        
-        status = "0_false"
-        isPrivate = param['isPrivate']
-        if(isPrivate):
-            status = "0_true"
+        mEvent = Event(**param)
+        mEvent.createStatusFromIsPrivate()
 
-        table = dynamodb.Table(eventTableName)
-        table.update_item(
-            Key = {
-                "eventId" : eventId
-            },
-            UpdateExpression = "set urlData=:a,#a=:b,#b=:c,university=:d,eventName=:e,price=:f,#c=:g,qualification=:h,detail=:i,contact=:j,#d=:k",
-            ExpressionAttributeNames = {
-                '#a' : "start",
-                '#b' : "end",
-                '#c' : "location",
-                '#d' : "status"
-            },
-            ExpressionAttributeValues = {
-                ':a' : param['urlData'],
-                ':b' : param['start'],
-                ':c' : end,
-                ':d' : param['university'],
-                ':e' : param['eventName'],
-                ':f' : param['price'],
-                ':g' : param['location'],
-                ':h' : param['qualification'],
-                ':i' : param['detail'],
-                ':j' : param['contact'],
-                ':k' : status
-            }
-        )
+        eventTable = EventTable(event)
+        eventTable.change(mEvent)
 
-        res = {
-            "eventId" : eventId
-        }
-        return {
-            'statusCode' : 200,
-            'headers' : {
-                'content-type' : 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            },
-            'body' : json.dumps(res)
-        }
+        res = { "eventId" : eventId }
+        return Successed(res)
     
     except:
         import  traceback
         traceback.print_exc()
-        res_error = {
-            "result" : 0
-        }
-        return {
-            'statusCode' : 500,
-            'headers' : {
-                'content-type' : 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            },
-            'body' : json.dumps(res_error)
-        }
+        Failured()
