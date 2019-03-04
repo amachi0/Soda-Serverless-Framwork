ccf32e5003531f01d656808671f4ca60337432e1
天知 <shunsuke.kmkz0617@gmail.com>
2019-03-04 17:30:15 +0900

    :recycle: リファクタリングを行なった

--- app/functions/api_gateway/event/create_event.py
+++ app/functions/api_gateway/event/create_event.py
@@ -2,115 +2,46 @@ import os
 import json
 import boto3
 import time
-from app.util.decimalencoder import DecimalEncoder
-from datetime import datetime as dt
-
-dynamodb = boto3.resource('dynamodb')
-sequenceTableName = os.environ['SEQUENCE_TABLE']
-eventTableName = os.environ['EVENT_TABLE']
-profileTableName = os.environ['PROFILE_TABLE']
-
-def next_seq(table, tableName):
-    res = table.update_item(
-        Key = {
-            'tableName' : tableName
-        },
-        UpdateExpression = "set seq = seq + :val",
-        ExpressionAttributeValues = {
-            ':val' : 1
-        },
-        ReturnValues="UPDATED_NEW"
-    )
-    return res['Attributes']['seq']
+from app.data.event import Event
+from app.data.source.sequence_table import SequenceTable
+from app.data.source.event_table import EventTable
+from app.data.source.profile_table import ProfileTable
+from app.util.return_dict import Successed, Failured
 
 def create_event(event, context):
     try:
         param = json.loads(event['body'])
-        seqtable = dynamodb.Table(sequenceTableName)
-        nextseq = next_seq(seqtable, 'event')
+
         identityId = param['identityId']
 
-        if not("end" in param):
-            end = None
-        else:
-            end = param['end']
-        
+        mEvent = Event(**param)
+        mEvent.createStatusFromIsPrivate()
+
+        sequenceTable = SequenceTable(event)
+        nextseq = sequenceTable.next_seq()
+        mEvent.eventId = nextseq
+
         now = int(time.time())
+        mEvent.updateTime = now
 
-        isPrivate = param['isPrivate']
-        if(isPrivate is False):
-            status = "0_false"
-        else:
-            status = "0_true"
+        eventTable = EventTable(event)
+        eventTable.insert(mEvent)
 
-        eventTable = dynamodb.Table(eventTableName)
-        eventTable.put_item(
-            Item = {
-                'identityId' : identityId,
-                'eventId' : nextseq,
-                'eventName' : param['eventName'],
-                'urlData' : param['urlData'],
-                'university' : param['university'],
-                'price' : param['price'],
-                'location' : param['location'],
-                'start' : param['start'],
-                'end' : end,
-                'qualification' : param['qualification'],
-                'detail' : param['detail'],
-                'contact' : param['contact'],
-                # 'entry' : param['entry'],
-                # 'sponsor' : param['sponsor'],
-                'status' : status,
-                'updateTime' : now,
-                'countOfLike' : 0
-            }
-        )
+        profileTable = ProfileTable(event)
+        item = profileTable.getFromIdentityId(identityId, "myEvent")
+        listMyEvent = []
+        if("myEvent" in item):
+            listMyEvent = item['myEvent']
+        listMyEvent.append(mEvent.eventId)
+
+        profileTable.changeMyEvent(identityId, listMyEvent)
         
-        profileTable = dynamodb.Table(profileTableName)
-        itemProfile = profileTable.get_item(
-            Key = {
-                "identityId" : identityId
-            }
-        )
-        
-        listMyevent = []
-        if("myEvent" in itemProfile['Item']):
-            listMyevent = itemProfile['Item']['myEvent']
-            
-        listMyevent.append(nextseq)
-        profileTable.update_item(
-            Key = {
-                "identityId" : identityId
-            },
-            UpdateExpression = "set myEvent=:x",
-            ExpressionAttributeValues = {
-                ':x' : listMyevent
-            }
-        )
-
         res = {
-            "eventId" : nextseq
+            "eventId" : mEvent.eventId
         }
-        return {
-            'statusCode' : 200,
-            'headers' : {
-                'content-type' : 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            },
-            'body' : json.dumps(res,cls=DecimalEncoder)
-        }
+        return Successed(res)
     
     except:
         import  traceback
         traceback.print_exc()
-        res_error = {
-            "result" : 0
-        }
-        return {
-            'statusCode' : 500,
-            'headers' : {
-                'content-type' : 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            },
-            'body' : json.dumps(res_error)
-        }
+        return Failured()
