1fad6ee4251ba5035823bde184f4337409801e56
天知 <shunsuke.kmkz0617@gmail.com>
2019-03-06 19:49:24 +0900

    :recycle: 大幅なリファクタリング

--- app/functions/api_gateway/session/check_soda_id.py
+++ app/functions/api_gateway/session/check_soda_id.py
@@ -1,61 +1,21 @@
-import json
-import boto3
-import os
-from boto3.dynamodb.conditions import Key, Attr
-
-dynamodb = boto3.resource('dynamodb')
-profileTableName = os.environ['PROFILE_TABLE']
-checkSodaIdIndex = os.environ['PROFILE_CHECK_SODA_ID_INDEX']
+from app.data.source.profile_table import ProfileTable
+from app.util.return_dict import Successed, Failured
 
 def check_soda_id(event, context):
 	try:
 		sodaId = event['queryStringParameters']['sodaId']
 
-		table = dynamodb.Table(profileTableName)
-		items = table.query(
-			IndexName = checkSodaIdIndex,
-			KeyConditionExpression = Key('sodaId').eq(sodaId)
-		)
-		
-		if (items['Count'] == 0):
-			res = {
-				"result" : True
-			}
-			return {
-				'statusCode' : 200,
-				'headers' : {
-					'content-type' : 'application/json',
-					'Access-Control-Allow-Origin': '*'
-				},
-				'body' : json.dumps(res)
-			}
+		profileTable = ProfileTable(event)
+		isValidSodaId = profileTable.isValidSodaId(sodaId)
 
+		if isValidSodaId:
+			res = { "result" : True }
 		else:
-			res = {
-				"result" : False
-			}
-			return {
-				'statusCode' : 200,
-				'headers' : {
-					'content-type' : 'application/json',
-					'Access-Control-Allow-Origin': '*'
-				},
-				'body' : json.dumps(res)
-			}
+			res = { "result" : False }
 
+		return Successed(res)
 
 	except:
 		import traceback
 		traceback.print_exc()
-		res_error = {
-			"result" : 0
-		}
-		return {
-		    'statusCode' : 500,
-		    'headers' : {
-		        'content-type' : 'application/json',
-		        'Access-Control-Allow-Origin': '*'
-		    },
-		    'body' : json.dumps(res_error)
-		}
-
+		return Failured()
