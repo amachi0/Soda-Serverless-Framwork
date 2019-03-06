bc1b18a892d167c734361c502478f83300f9ec29
天知 <shunsuke.kmkz0617@gmail.com>
2019-03-07 00:58:22 +0900

    :recycle: ビジネスロジックを分離

---
+++ app/logic/logic_created_event.py
@@ -0,0 +1,25 @@
+from app.data.event import Event
+
+def createResponseFromEvents(events, startNum,favoriteEvents):
+    res = {}
+    for event in events:
+        if(event.eventId in favoriteEvents):
+                isFavorite = True
+        else:
+            isFavorite = False
+
+        res[startNum] = {
+            'eventId' : event.eventId,
+            'eventName' : event.eventName,
+            'updateTime' : event.updateTime,
+            'startTime' : event.start,
+            'endTime' : event.end,
+            'location' : event.location,
+            'urlData' : event.urlData,
+            'university' : event.university,
+            'countOfLike' : event.countOfLike,
+            'isFavorite' : isFavorite
+        }
+        startNum += 1
+    
+    return res
