751f9ff03de6ca63392bb2f8fbd4f99d23bab00a
天知 <shunsuke.kmkz0617@gmail.com>
2019-03-06 22:43:47 +0900

    :recycle: upload_binaryをリファクタリング

--- app/data/source/s3.py
+++ app/data/source/s3.py
@@ -1,10 +1,14 @@
 import json
 import boto3
 import os
+import base64
+import uuid
 
 class S3():
     def __init__(self):
         self.s3 = boto3.resource('s3')
+        self.imageBucketName = os.environ["S3_IMAGE_BUCKET"]
+        self.arnS3 = os.environ['ARN_S3']
     
     def getJson(self, file):
         if file == 'faqs':
@@ -21,3 +25,17 @@ class S3():
         content = obj.get()['Body'].read().decode('utf-8')
         contentJson = json.loads(content)
         return contentJson
+    
+    def uploadBinary(self, binary):
+        imageBody = base64.b64decode(binary)
+        imageBucket = self.s3.Bucket(self.imageBucketName)
+        imageId = str(uuid.uuid4())
+        key = imageId + ".png"
+        url = self.arnS3 + "/" + self.imageBucketName + "/" + key
+        imageBucket.put_object(
+            ACL = "public-read",
+            Body = imageBody,
+            Key = key,
+            ContentType = "image/png"
+            )
+        return url
--- app/functions/api_gateway/upload_binary/upload_binary.py
+++ app/functions/api_gateway/upload_binary/upload_binary.py
@@ -1,53 +1,16 @@
-import json
-import boto3
-import base64
-import uuid
-import os
-
-s3 = boto3.resource('s3')
-bucketName = os.environ["S3_IMAGE_BUCKET"]
-arnS3 = os.environ['ARN_S3']
+from app.data.source.s3 import S3
+from app.util.return_dict import Successed, Failured
 
 def upload_binary(event, context):
     try:
-        print(event)
-        bucket = s3.Bucket(bucketName)
-        #param = json.loads(event['body'])
-        imageBody = base64.b64decode(event['body'])
-        imageId = str(uuid.uuid4())
-        key = imageId + ".png"
-        url = arnS3 + "/" + bucketName + "/" + key
-        bucket.put_object(
-            ACL = "public-read",
-            Body = imageBody,
-            Key = key,
-            ContentType = "image/png"
-            )
-        
-        res = {
-            "url" : url
-        }
-        print(url)
-        return{
-            'statusCode' : 200,
-            'headers' : {
-                'content-type' : 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            },
-            'body' : json.dumps(res)
-        }
+        binary = event['body']
+        s3 = S3()
+        url = s3.uploadBinary(binary)
+        res = { "url" : url }
+
+        return Successed(res)
     
     except:
         import traceback
         traceback.print_exc()
-        res_error = {
-            "result" : 0
-        }
-        return {
-            'statusCode' : 500,
-            'headers' : {
-                'content-type' : 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            },
-            'body' : json.dumps(res_error)
-        }
+        return Failured()
