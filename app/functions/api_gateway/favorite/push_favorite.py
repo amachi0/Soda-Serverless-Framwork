228691d8f02f83fccfdcf8aa9204badda78d3ad9
天知 <shunsuke.kmkz0617@gmail.com>
2019-03-06 21:36:39 +0900

    :recycle: 大幅なリファクタリング

--- app/functions/api_gateway/favorite/push_favorite.py
+++ app/functions/api_gateway/favorite/push_favorite.py
@@ -1,86 +1,33 @@
 import json
-import boto3
-import os
-from app.util.decimalencoder import DecimalEncoder
+from app.data.source.profile_table import ProfileTable
+from app.data.source.event_table import EventTable
+from app.util.return_dict import Successed, Failured
 
-dynamodb = boto3.resource('dynamodb')
-profileTableName = os.environ['PROFILE_TABLE']
-eventTableName = os.environ['EVENT_TABLE']
+''' パラメーター
+{
+    "identityId": "amachi1",
+    "eventId" : 1 
+}
+'''
 
 def push_favorite(event, context):
     try:
         body = json.loads(event['body'])
         eventId = int(body["eventId"])
         identityId = body["identityId"]
-        profileTable = dynamodb.Table(profileTableName)
-        itemsProfile = profileTable.get_item(
-            Key = {
-                "identityId" : identityId
-            }
-        )
-        listEventId = []
-        if("favoriteEvent" in itemsProfile['Item']):
-            listEventId = itemsProfile['Item']['favoriteEvent']
+        listEventId = [eventId]
+        listIdentityId = [identityId]
 
-        listEventId.append(eventId)
-        
-        profileTable.update_item(
-            Key = {
-                "identityId" : identityId
-            },
-            UpdateExpression = "set favoriteEvent=:x",
-            ExpressionAttributeValues = {
-                ':x' : listEventId
-            }
-        )
-        
-        eventTable = dynamodb.Table(eventTableName)
-        itemsEvent = eventTable.get_item(
-            Key = {
-                "eventId" : eventId
-            }
-        )
-        listIdentityId = []
-        if('favorite' in itemsEvent['Item']):
-            listIdentityId = itemsEvent['Item']['favorite']
-        listIdentityId.append(identityId)
-        
-        countOfLike = len(listIdentityId)
-        
-        eventTable.update_item(
-            Key = {
-                "eventId" : eventId
-            },
-            UpdateExpression = "set favorite=:x, countOfLike=:y",
-            ExpressionAttributeValues = {
-                ':x' : listIdentityId,
-                ':y' : countOfLike
-            }
-        )
+        profileTable = ProfileTable(event)
+        profileTable.addListItemInProfileTable(identityId, 'favoriteEvent', listEventId)
 
-        res = {
-            "result" : 1
-        }
-        return {
-            'statusCode' : 200,
-            'headers' : {
-                'content-type' : 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            },
-            'body' : json.dumps(res)
-        }
+        eventTable = EventTable(event)
+        eventTable.addFavorite(eventId, listIdentityId)
+
+        res = { "result" : 1 }
+        return Successed(res)
         
     except:
         import traceback
         traceback.print_exc()
-        resError = {
-            "result" : 0
-        }
-        return {
-            'statusCode' : 500,
-            'headers' : {
-                'content-type' : 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            },
-            'body' : json.dumps(resError)
-        }
+        Failured()
