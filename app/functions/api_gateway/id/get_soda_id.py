2c48fd591ad78b736f43ba71852e223ff4acca67
天知 <shunsuke.kmkz0617@gmail.com>
2019-03-06 17:10:05 +0900

    :recycle: get_soda_idのリファクタリング

--- app/functions/api_gateway/id/get_soda_id.py
+++ app/functions/api_gateway/id/get_soda_id.py
@@ -1,60 +1,20 @@
-import json
-import boto3
-import os
-
-dynamodb = boto3.resource('dynamodb')
-profileTableName = os.environ['PROFILE_TABLE']
+from app.data.source.profile_table import ProfileTable
+from app.util.return_dict import Successed, Failured
 
 def get_soda_id(event, context):
 	try:
 		param = event["queryStringParameters"]
 		identityId = param['identityId']
 
-		table = dynamodb.Table(profileTableName)
-		item = table.get_item(
-		    Key = {
-		        "identityId" : identityId
-		    }
-		)
-
-		if("Item" in item):
-			sodaId = item['Item']['sodaId']
-			res = {
-			    "sodaId" : sodaId
-			}
-			return {
-			    'statusCode' : 200,
-			    'headers' : {
-			        'content-type' : 'application/json',
-			        'Access-Control-Allow-Origin': '*'
-			    },
-			    'body' : json.dumps(res)
-			}
-
-		else:
-			res_error = {
-			    "result" : 0
-			}
-			return {
-			    'statusCode' : 200,
-			    'headers' : {
-			        'content-type' : 'application/json',
-			        'Access-Control-Allow-Origin': '*'
-			    },
-			    'body' : json.dumps(res_error)
-			}
+		profileTable = ProfileTable(event)
+		profile = profileTable.getFromIdentityId(identityId, 'sodaId')
+		
+		res = {
+			"sodaId" : profile.sodaId
+		}
+		return Successed(res)
 
 	except:
 		import  traceback
 		traceback.print_exc()
-		res_error = {
-		    "result" : 0
-		}
-		return {
-		    'statusCode' : 500,
-		    'headers' : {
-		        'content-type' : 'application/json',
-		        'Access-Control-Allow-Origin': '*'
-		    },
-		    'body' : json.dumps(res_error)
-		}
+		return Failured()
