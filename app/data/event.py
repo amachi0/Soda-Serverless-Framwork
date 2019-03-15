6db1f9d482bb29c6238ae132ab85754f06c64bc5
天知 <shunsuke.kmkz0617@gmail.com>
2019-03-15 15:18:20 +0900

    :memo: sponserの綴りをsponsorに修正

--- app/data/event.py
+++ app/data/event.py
@@ -2,7 +2,7 @@ class Event():
     def __init__(self, identityId=None, eventName=None, urlData=None, university=None,
                 price=None, location=None, start=None, qualification=None, detail=None,
                 contact=None, countOfLike=0, end=None, eventId=0, status="",
-                isPrivate=False, updateTime=0,  favorite=[], sponser=None, entry=None):
+                isPrivate=False, updateTime=0,  favorite=[], sponsor=None, entry=None):
         self.contact = contact
         self.countOfLike = countOfLike
         self.detail = detail
@@ -20,7 +20,7 @@ class Event():
         self.university = university
         self.updateTime = updateTime
         self.urlData = urlData
-        self.sponser = sponser
+        self.sponsor = sponsor
         self.entry = entry
     
     def __lt__(self, other):
