08ee345662b8f244d653f2088bf223d198d7c052
天知 <shunsuke.kmkz0617@gmail.com>
2019-03-26 16:41:00 +0900

    :sparkles: 今日開催されるイベント情報を取ってきてツイートするLambda関数を作成

---
+++ app/functions/schedule/tweet_event_info.py
@@ -0,0 +1,29 @@
+import time
+import decimal
+from botocore.exceptions import ClientError
+from app.data.twitter import Twitter
+from app.data.source.event_table import EventTable
+from app.util.return_dict import Successed, Failured
+
+SECOND_OF_ONE_DAY = 86400
+
+
+def tweet_event_info(event, context):
+    try:
+        now = time.time()
+        nowDecimal = decimal.Decimal(str(now))
+        timeAfterDay = nowDecimal + SECOND_OF_ONE_DAY
+
+        eventTable = EventTable(event)
+        events = eventTable.queryForTweet(timeAfterDay)
+        print(events)
+
+        twitter = Twitter()
+        twitter.tweetEventInfoFromEvents(events)
+
+        res = {'result': 1}
+        return Successed(res)
+
+    except ClientError:
+        import traceback
+        return Failured(traceback.format_exc())
