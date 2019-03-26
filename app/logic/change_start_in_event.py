2639204eec4e81ae69cd326d0107672d42d50672
天知 <shunsuke.kmkz0617@gmail.com>
2019-03-26 16:48:47 +0900

    :sparkles: Eventクラスを引数に受け取り、そのstartとendから日本時間の文字列を返す関数を作成

--- app/logic/change_start_in_event.py
+++ app/logic/change_start_in_event.py
@@ -17,3 +17,27 @@ def changeStartInEvent(event=Event):
     date = str(month) + '/' + str(date) + strWeekDay
     print(date)
     event.start = date
+
+
+def getStrFromStartAndEndInEvent(event=Event):
+    JST = timezone(timedelta(hours=+9), 'JST')
+    startDateTime = datetime.fromtimestamp(event.start, JST)
+
+    startStrHour = str(startDateTime.hour)
+    startStrMinute = str(startDateTime.minute)
+    if len(startStrMinute) < 2:
+        startStrMinute = '0' + startStrMinute
+
+    timeStr = startStrHour + ':' + startStrMinute + '〜'
+
+    if event.end:
+        endDateTime = datetime.fromtimestamp(event.end, JST)
+
+        endStrHour = str(endDateTime.hour)
+        endStrMinute = str(endDateTime.minute)
+        if len(endStrMinute) < 2:
+            endStrMinute = '0' + endStrMinute
+
+        timeStr += endStrHour + ":" + endStrMinute
+
+    return timeStr
