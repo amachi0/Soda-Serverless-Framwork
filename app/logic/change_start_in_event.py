57c9d92df49c052f4ce4906f0a739d04aae5ef7a
天知 <shunsuke.kmkz0617@gmail.com>
2019-03-26 16:45:19 +0900

    :shirt: ダブルクォーテーションをシングルクォーテーションに変更

--- app/logic/change_start_in_event.py
+++ app/logic/change_start_in_event.py
@@ -1,7 +1,7 @@
 from app.data.event import Event
 import datetime
 
-dayOfTheWeek = ["(月)", "(火)", "(水)", "(木)", "(金)", "(土)", "(日)"]
+dayOfTheWeek = ['(月)', '(火)', '(水)', '(木)', '(金)', '(土)', '(日)']
 
 
 def changeStartInEvent(event=Event):
