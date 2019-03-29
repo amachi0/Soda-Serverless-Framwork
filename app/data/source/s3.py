f09ff35d4fb20e9d8cdb0bb9bdd0ae1c70c40b37
天知 <shunsuke.kmkz0617@gmail.com>
2019-03-29 14:08:17 +0900

    :recycle: ビジネスロジックを分離

--- app/data/source/s3.py
+++ app/data/source/s3.py
@@ -2,7 +2,8 @@ import json
 import boto3
 import os
 import base64
-import uuid
+from app.logic.logic_s3 \
+    import setBucketAndFileName, makeImageUrl
 
 
 class S3():
@@ -12,16 +13,7 @@ class S3():
         self.arnS3 = os.environ['ARN_S3']
 
     def getJson(self, file):
-        if file == 'faqs':
-            bucket = os.environ['S3_JSON_BUCKET']
-            fileName = os.environ['FAQS_JSON']
-        elif file == 'terms':
-            bucket = os.environ['S3_JSON_BUCKET']
-            fileName = os.environ['TERMS_JSON']
-        elif file == 'university':
-            bucket = os.environ['S3_JSON_BUCKET']
-            fileName = os.environ['UNIVERSITY_JSON']
-
+        bucket, fileName = setBucketAndFileName(file)
         obj = self.s3.Object(bucket, fileName)
         content = obj.get()['Body'].read().decode('utf-8')
         contentJson = json.loads(content)
@@ -30,9 +22,8 @@ class S3():
     def uploadBinary(self, binary):
         imageBody = base64.b64decode(binary)
         imageBucket = self.s3.Bucket(self.imageBucketName)
-        imageId = str(uuid.uuid4())
-        key = imageId + ".png"
-        url = self.arnS3 + "/" + self.imageBucketName + "/" + key
+
+        key, url = makeImageUrl(self.arnS3, self.imageBucketName)
         imageBucket.put_object(
             ACL="public-read",
             Body=imageBody,
