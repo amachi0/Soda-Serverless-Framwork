f892b941bdcc1838430f55b40f0eb6c3e2ac3c03
天知 <shunsuke.kmkz0617@gmail.com>
2019-03-29 14:10:03 +0900

    :up: create_response_from_eventsとcreate_startnum_and_sizeファイルを一つのlogic_timelineファイルに変更

--- app/logic/create_startnum_and_size.py
+++
@@ -1,9 +0,0 @@
-def createStartNumAndSize(page):
-    if page == 0:
-        startNum = 0
-        size = 3
-    else:
-        startNum = 3 + (page - 1) * 5
-        size = 5
-
-    return startNum, size
